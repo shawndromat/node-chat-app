b706c80601b431bddb8a17223f3f90f504ed9ceb
AppAcademy student <contact@appacademy.io>
2014-05-20 16:26:48 -0700

    basic chat/emit

--- router.js
+++ router.js
@@ -1,14 +1,18 @@
-var fs = require('fs');
+var fs = require('fs'),
+    mime = require('mime');
 
 function route(pathname, response) {
     console.log("About to route a request for " + pathname);
 
     if (pathname === "/") {
+      response.setHeader('Content-Type', 'text/html');
       fs.readFile('public/index.html', function (err, data) {
         serve(err, data, response)
       });
     } else {
-      fs.readFile(pathname,function (err, data) {
+      response.setHeader('Content-Type', mime.lookup('public' + pathname));
+      fs.readFile('public' + pathname, function (err, data) {
+        console.log('serving ' + err)
         serve(err, data, response)
       });
     }
@@ -23,13 +27,13 @@ function serve(err, data, response) {
 }
 
 function serveData(data, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
+  response.writeHead(200);
   response.write(data);
   response.end();
 }
 
 function serveError(response) {
-  response.writeHead(404, {"Content-Type": "text/plain"});
+  response.writeHead(404);
   response.write("Content not found");
   response.end();
 }
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var route = require('./router');
+var chatServer = require('./lib/chat_server');
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+var app = http.createServer(function (request, response) {
   console.log(request.url);
   route.route(request.url, response);
   // response.end('Hello World\n');
 }).listen(8080);
+
+chatServer.createChat(app);
 
 console.log('Server running at http://127.0.0.1:8080/');
